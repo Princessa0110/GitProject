c0eb8a17f69836c84788e95a8fe2fa3d052e745b
Princessa0110 <127737226+Princessa0110@users.noreply.github.com>
2023-04-09 13:53:44 +0300

    first commit

---
+++ Assets/Scripts/FPCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPCamera : MonoBehaviour
+{
+    [SerializeField] private float _sensivity;
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _verticalLover;
+    [SerializeField] private float _verticalUpper;
+    private float _currentVerticalAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Mouse X") * _sensivity * Time.deltaTime;
+        var vertical = -Input.GetAxis("Mouse Y") * _sensivity * Time.deltaTime;
+
+        _currentVerticalAngle = Mathf.Clamp(_currentVerticalAngle + vertical, _verticalUpper, _verticalLover);
+        transform.localRotation = Quaternion.Euler(_currentVerticalAngle, 0, 0);
+
+        _player.Rotate(0, horizontal, 0);
+    }
+}
