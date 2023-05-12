493ccfba6dc479150802bec61dee13ac49da1478
Princessa0110 <127737226+Princessa0110@users.noreply.github.com>
2023-05-12 13:17:27 +0300

    first commit

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private Shooting _currentWeapon;
+    
+    private bool _triggerPressed;
+
+    private InputDevice _rightHand;
+    
+    private void Update()
+    {
+        _rightHand = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        if (_rightHand.TryGetFeatureValue(CommonUsages.triggerButton, out var triggerValue) && triggerValue && !_triggerPressed)
+        {
+            _triggerPressed = true;
+
+            _currentWeapon.ShootBullet();
+        }
+        else if (!triggerValue)
+        {
+            _triggerPressed = false;
+        }
+    }
+
+    public void SetUpCurrentWeapon(Shooting shooting)
+    {
+        _currentWeapon = shooting;
+    }
+}
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,92 +1,93 @@
+using System;
+using TMPro;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
 
 public class Shooting : MonoBehaviour
 {
     public int maxammo;
     private int currentammo;
 
-    public AudioClip reloadSound;
-    public AudioClip shootSound;
+    [SerializeField] private PlayerShoot _playerShoot;
+    [SerializeField] private TMPro.TextMeshPro _ammoText;
 
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float bulletSpeed;
     public float bulletLifetime = 2f;
 
-    private bool triggerPressed = false;
     private float timer = 0f;
     private float reloadTime = 5f;
 
     private bool _isReloading = false;
+    private bool _isReloaded;
 
-    private AudioSource audioSource;  
-    public TMPro.TextMeshPro ammoText;
-    void Start()
-{
-    audioSource = GetComponent<AudioSource>();
-    audioSource.playOnAwake = false;
-
-    currentammo = maxammo;
-}
-
-    void Update()
+    private void Start()
     {
+        UpdateAmmotext();
+    }
+
+    private void Update()
+    {
+        if (currentammo <= 0 && !_isReloading)
+        {
+            Reload();
+        }
+        
         if (_isReloading)
         {
             timer += Time.deltaTime;
 
             if (!(timer >= reloadTime))
             {
+                _isReloaded = true;
                 return;
             }
-   
-                
-            currentammo = maxammo;
-            _isReloading = false;
+        }
 
-            timer = 0;
-
+        if (!_isReloaded)
+        {
             return;
         }
-
-        var device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
         
-        if (device.TryGetFeatureValue(CommonUsages.triggerButton, out var triggerValue) && triggerValue && !triggerPressed)
-        {
-            triggerPressed = true;
-
-            if (currentammo <= 0 && !_isReloading)
-            {
-                Reload();
-            }
-            
-            ShootBullet();
-        }
-        else if (!triggerValue)
-        {
-            triggerPressed = false;
-        }
+        currentammo = maxammo;
+        
+        _isReloading = false;
+        _isReloaded = false;
+        
+        timer = 0;
+        
+        UpdateAmmotext();
     }
-
+    
     private void Reload()
     {
         _isReloading = true;
-        audioSource.PlayOneShot(reloadSound);
     }
-
-    void ShootBullet()
+    
+    public void ShootBullet()
     {
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (_isReloading)
+        {
+            return;
+        }
+        
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        var rb = bullet.GetComponent<Rigidbody>();
         rb.velocity = bulletSpawn.forward * bulletSpeed;
         Destroy(bullet, bulletLifetime);
         currentammo--;
-        audioSource.PlayOneShot(shootSound);
+        
+        UpdateAmmotext();
+    }
+    
+    public void SetUp()
+    {
+        _playerShoot.SetUpCurrentWeapon(this);
     }
 
-    
+    private void UpdateAmmotext()
+    {
+        _ammoText.text = $"Ammo: {maxammo}";
+    }
 }
